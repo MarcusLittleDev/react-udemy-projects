a8c9b1454e54e94b57671085fc5581a7dfd52098
Little <LittleMa@ad.reyrey.com>
2019-02-22 14:57:46 -0600

    finish burger builder beginning section

--- burger-builder-app/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder-app/src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,9 +3,9 @@ import React, {Component} from 'react';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
-    // componentWillUpdate() {
-    //     console.log("[Order Summary] will update")
-    // }
+    componentWillUpdate() {
+        console.log("[Order Summary] will update")
+    }
 
     render () {
         const ingredientSummary = Object.keys(this.props.ingredients)
