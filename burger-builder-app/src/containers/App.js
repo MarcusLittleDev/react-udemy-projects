da40356916428511f9bdce7f1b2d47e7c3331f75
Little <LittleMa@ad.reyrey.com>
2019-03-21 13:37:31 -0500

    ceated orders page

--- burger-builder-app/src/containers/App.js
+++ burger-builder-app/src/containers/App.js
@@ -5,6 +5,7 @@ import './App.module.css';
 import Layout from '../components/Layout/Layout'
 import BurgerBuilder from '../containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './Checkout/Checkout';
+import Orders from './Orders/Orders';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         <Layout>
           <Switch>
             <Route path="/checkout" component={Checkout}/>
+            <Route path='/orders' componetn={Orders} />
             <Route path="/" exact component={BurgerBuilder}/>
           </Switch>
         </Layout>
