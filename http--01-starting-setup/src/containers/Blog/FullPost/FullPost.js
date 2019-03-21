fe3f5d19f08968026832bdec190301bd75cd694d
Little <LittleMa@ad.reyrey.com>
2019-03-21 10:38:03 -0500

    finished routing section

--- http--01-starting-setup/src/containers/Blog/FullPost/FullPost.js
+++ http--01-starting-setup/src/containers/Blog/FullPost/FullPost.js
@@ -8,10 +8,18 @@ class FullPost extends Component {
         loadedPost: null
     }
 
+    componentDidMount() {
+        this.loadData();
+    }
+
     componentDidUpdate() {
-        if (this.props.id){
-            if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
-                axios.get('/posts/' + this.props.id)
+        this.loadData();
+    }
+
+    loadData() {
+        if (this.props.match.params.id){
+            if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id != this.props.match.params.id)) {
+                axios.get('/posts/' + this.props.match.params.id)
                     .then(response => {
                         this.setState({loadedPost:response.data})
                     })
@@ -20,17 +28,14 @@ class FullPost extends Component {
     }
 
     deletePostHandler = () => {
-        axios.delete('/posts/' + this.props.id)
+        axios.delete('/posts/' + this.props.match.params.id)
             .then(response => {
                 console.log(response)
             })
     }
+    
     render () {
-        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
-        if(this.props.id)
-        {
-            post = <p style={{textAlign: 'center'}}>Loading...</p>;
-        }
+        let post = <p style={{textAlign: 'center'}}>Loading...</p>;
         if(this.state.loadedPost) {
             post = (
                 <div className="FullPost">
@@ -39,7 +44,7 @@ class FullPost extends Component {
                     <div className="Edit">
                         <button 
                             className="Delete"
-                            onClick={() => this.deletePostHandler(this.props.id)}>Delete</button>
+                            onClick={() => this.deletePostHandler(this.props.match.params.id)}>Delete</button>
                     </div>
                 </div>
 
--- http--01-starting-setup/src/containers/Blog/NewPost/NewPost.js
+++ http--01-starting-setup/src/containers/Blog/NewPost/NewPost.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios'
+import axios from 'axios';
 
 import './NewPost.css';
 
@@ -7,9 +7,12 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Max',
     }
 
+    componentDidMount () {
+        console.log(this.props)
+    }
     postDataHandler = () => {
         const data = {
             title: this.state.title,
@@ -20,6 +23,7 @@ class NewPost extends Component {
         axios.post('/posts', data)
             .then(response => {
                 console.log(response)
+                this.props.history.replace('/posts')
             })
     }
 
