c35201b50822122e84e1368daeabb263d25d456c
Little <LittleMa@ad.reyrey.com>
2019-03-21 17:01:32 -0500

    started adding form validation

--- burger-builder-app/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder-app/src/containers/Checkout/ContactData/ContactData.js
@@ -15,7 +15,12 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Name'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             email: {
                 elementType: 'input',
@@ -23,7 +28,12 @@ class ContactData extends Component {
                     type: 'email',
                     placeholder: 'Email'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             street: {
                 elementType: 'input',
@@ -31,7 +41,12 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Street'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             zipCode: {
                 elementType: 'input',
@@ -39,7 +54,14 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Zip Code'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5
+                },
+                valid: false,
+                touched: false
             },
             country: {
                 elementType: 'input',
@@ -47,7 +69,12 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Country'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -80,7 +107,7 @@ class ContactData extends Component {
             orderData: formData,
             
         }
-        
+
         axios.post('/orders.json', order)
         .then(response => {
             this.setState({loading:false});
@@ -98,23 +125,49 @@ class ContactData extends Component {
             ...this.state.orderForm,
             [id]: {
                 ...this.state.orderForm[id],
-                value: event.target.value
+                value: event.target.value,
+                valid:this.validateForm(event.target.value, this.state.orderForm[id].validation),
+                touched:true,
             }
         };
+
         
         this.setState({orderForm})
+    }
+
+    validateForm(value, rules) {
+        let isValid = true;
+
+        if(rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if (rules.maxLength)
+        {
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+
+        return isValid;
     }
 
     render () {
         const formElementsArray = [];
         for (let key in this.state.orderForm){
+            const formElement = this.state.orderForm[key];
             formElementsArray.push(
                 <Input
                     key={key} 
-                    elementType={this.state.orderForm[key].elementType}
-                    elementConfig={this.state.orderForm[key].elementConfig} 
-                    value={this.state.orderForm[key].value}
-                    changed={(event) => this.inputChandedHandler(event, key)}/>
+                    elementType={formElement.elementType}
+                    elementConfig={formElement.elementConfig} 
+                    value={formElement.value}
+                    changed={(event) => this.inputChandedHandler(event, key)}
+                    invalid={!formElement.valid}
+                    shouldValidate={formElement.validation}
+                    touched={formElement.touched}/>
             )
         }
 
