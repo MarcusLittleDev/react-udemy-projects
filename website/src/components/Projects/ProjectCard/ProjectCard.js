fea006ecf8899d6a568ddc4316dbce808e2b0791
Little <LittleMa@ad.reyrey.com>
2019-04-30 16:11:57 -0500

    project page styling

--- website/src/components/Projects/ProjectCard/ProjectCard.js
+++ website/src/components/Projects/ProjectCard/ProjectCard.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import classes from "./ProjectCard.module.css";
 
 const projectCard = props => (
-  <Card className="bg-dark" text-white>
+  <Card className={classes.ProjectCard} text-white>
     <Card.Img
       src={"http://placehold.it/100x100&text=" + props.title}
       alt={props.title}
-      variant="top"
     />
-    <Card.Body>
-      <Card.Title>{props.title}</Card.Title>
-      <Card.Text>{props.description}</Card.Text>
-    </Card.Body>
+    <Card.ImgOverlay className={classes.ProjectCardImgOverlay}>
+      <Card.Title className={classes.ProjectCardTitle}>
+        {props.title}
+      </Card.Title>
+      <Card.Text className={classes.ProjectCardText}>
+        {props.description}
+      </Card.Text>
+    </Card.ImgOverlay>
   </Card>
 );
 
--- website/src/components/Projects/Projects.js
+++ website/src/components/Projects/Projects.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import ProjectCard from "./ProjectCard/ProjectCard";
+import classes from "./Projects.module.css";
 
 const projects = props => {
   let projects = props.projects.map(project => (
-    <Col sm={4} md={3}>
-      <ProjectCard
-        key={project.id}
-        title={project.title}
-        description={project.description}
-        image={project.image}
-      />
+    <Col sm={6} md={4} lg={3} className={classes.Projects}>
+      <Link to="#">
+        <ProjectCard
+          key={project.id}
+          title={project.title}
+          description={project.description}
+          image={project.image}
+        />
+      </Link>
     </Col>
   ));
 
